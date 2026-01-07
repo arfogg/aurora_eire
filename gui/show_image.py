3a7dc00e9e70d5e1cdefafbe18933e1542c07906
Alexandra Fogg <arfogg@cp.dias.ie>
2026-01-07 10:32:39 +0000

    comments and docstrings

--- gui/show_image.py
+++ gui/show_image.py
@@ -9,17 +9,13 @@ import os
 import sys
 import pathlib
 from PySide6.QtWidgets import (QApplication, QLabel, QPushButton,
-    QVBoxLayout, QWidget)
+                               QVBoxLayout, QWidget)
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt
 
-
 from auroral_image_class import Auroral_Image
 
-# image_path = os.path.join(
-#     "C:"+os.sep, r"Users\Alexandra\Documents\wind_waves_akr_code\aurora_eire",
-#     "aurora_eire_logo.png")
-# image_path = "aurora_eire_logo.png"
+
 image_dir = os.path.join(
     "C:"+os.sep, r"\Users\Alexandra\Documents\data\aurora_eire\test_images")
 
@@ -52,30 +48,6 @@ class ImageWindow(QLabel):
                 Qt.SmoothTransformation
             )
             self.setPixmap(scaled)
-
-
-# class ImageWindow(QLabel):
-#     # A generic window-like container
-
-#     def __init__(self, image_path):
-#         super().__init__()
-
-#         # Align image within window
-#         self.setAlignment(Qt.AlignCenter)
-#         # Store the image once
-#         self.pixmap_original = QPixmap(image_path)
-
-#     # Called whenever the window size changes
-#     def resizeEvent(self, event):
-#         if not self.pixmap_original.isNull():
-#             scaled = self.pixmap_original.scaled(
-#                 self.size(),
-#                 Qt.KeepAspectRatio,
-#                 Qt.SmoothTransformation
-#             )
-#             self.setPixmap(scaled)
-
-#         super().resizeEvent(event)
 
 
 class ImageViewer(QWidget):
@@ -124,54 +96,79 @@ class ImageViewer(QWidget):
         self.setLayout(layout)
 
         # Window title
-        self.setWindowTitle("Resizable Image Viewer")
+        self.setWindowTitle("Aurora Éire Image Viewer")
         # Initial window size
         self.resize(700, 700)
 
+        # Update buttons, title, etc
         self.image_change_updates()
 
-
     def update_image_title(self):
-        current = self.index + 1 # Going from 1 -> len
+        """
+        Update the image title.
+
+        Returns
+        -------
+        None.
+
+        """
+        current = self.index + 1  # Going from 1 -> len
         total = len(self.image_paths)
-        self.image_title.setText(f"Image {current} / {total}\n" + "ADD FILENAME HERE")
-    # def next_image(self):
-    #     # Function that steps through to next image
-    #     # Add 1 to the image and wrap around at the end
-    #     self.index = (self.index + 1) % len(self.image_paths)
-    #     # Load the new image
-    #     self.image_label.set_image(self.image_paths[self.index])
-    #     # self.image_label.pixmap_original = QPixmap(
-    #     #     self.image_paths[self.index]
-    #     # )
-    #     # # Bodge to force a redraw - needs cleaning up later
-    #     # self.image_label.resizeEvent(None)
+        self.image_title.setText(f"Image {current} / {total}\n"
+                                 + "ADD FILENAME HERE")
+
     def next_image(self):
+        """
+        Move to the next image.
+
+        Returns
+        -------
+        None.
+
+        """
         if self.index < len(self.image_paths) - 1:
             self.index += 1
             self.image_label.set_image(self.image_paths[self.index])
             self.image_change_updates()
 
-
-    # def previous_image(self):
-    #     # Function that steps through to previous image
-    #     # Add 1 to the image and wrap around at the end
-    #     self.index = (self.index - 1) 
-    #     # Load the new image
-    #     self.image_label.set_image(self.image_paths[self.index])
     def previous_image(self):
+        """
+        Move to the previous image.
+
+        Returns
+        -------
+        None.
+
+        """
         if self.index > 0:
             self.index -= 1
             self.image_label.set_image(self.image_paths[self.index])
             self.image_change_updates()
-        
+
     def update_buttons(self):
+        """
+        Updates the next and previous buttons.
+
+        Returns
+        -------
+        None.
+
+        """
         self.previous_button.setEnabled(self.index > 0)
         self.next_button.setEnabled(self.index < len(self.image_paths) - 1)
 
     def image_change_updates(self):
+        """
+        Updates buttons, text/labels, etc
+
+        Returns
+        -------
+        None.
+
+        """
         self.update_buttons()
         self.update_image_title()
+
 
 def list_images():
 
@@ -193,15 +190,6 @@ def main():
     image_paths = list_images()
 
     app = QApplication(sys.argv)
-
-    # label = ImageWindow(image_paths[0])
-
-    # label.setWindowTitle("Resizable Image Viewer")
-    # label.resize(600, 600)
-    # label.show()
-
-    # sys.exit(app.exec())
-
 
     viewer = ImageViewer(image_paths)
     viewer.show()
